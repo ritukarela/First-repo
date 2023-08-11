25cdf2e020bda087c2aa273692161f5e9f3de651
Ritu karela <ritu.karela@peak.ai>
2023-08-11 04:24:29 +0000

    Committing first change

---
+++ a.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+  int x = 0;
+  for(x=0; x<10; x++)
+      {
+        std::cout <<x;
+      }
+
+  return 0;
+}
+
+
+//int main() {
+  //  std::cout << "Hello World!";
+  // return 0;
+//}
