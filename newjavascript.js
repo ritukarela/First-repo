197d068784b4dc11699a02b1dbb6f578eb6453b4
Ritu karela <ritu.karela@peak.ai>
2023-08-22 07:23:15 +0000

    changes in the newbranch

--- newjavascript.js
+++ newjavascript.js
@@ -2,11 +2,10 @@
 const prompt = require("prompt-sync") ({sigint: true});
 const num1 = parseInt(prompt('Enter the first number '));
 const num2 = parseInt(prompt('Enter the second number '));
-const num3 = parseInt(prompt('Enter the third number '));
 
 
 //add two numbers
-const sum = num1 + num2 +num3;
+const sum = num1 + num2;
 
 // display the sum
-console.log(`The sum of ${num1} ${num2} and ${num3} is ${sum}`);
+console.log(`The sum of ${num1} and ${num2} is ${sum}`);
